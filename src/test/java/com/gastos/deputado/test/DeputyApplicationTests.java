56c7ae705a3e71758c2bed0e742da3dd4120ebb9
edgarpf <edgarpeixoto14@gmail.com>
2019-07-07 12:40:58 -0300

    unit test

--- src/test/java/com/gastos/deputado/test/DeputyApplicationTests.java
+++ src/test/java/com/gastos/deputado/test/DeputyApplicationTests.java
@@ -1,16 +1,31 @@
 package com.gastos.deputado.test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DeputyApplicationTests {
+import io.restassured.RestAssured;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DeputyApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Before
+	public void setup() {
+	    RestAssured.port = port;
+	}
+	
 	@Test
-	public void contextLoads() {
+	public void test() {
+        RestAssured.get("").
+        then().
+        statusCode(200);
 	}
 
 }
